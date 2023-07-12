f2c9fef6ee449ac1da9582983f37ea2788c38ae0
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-12 15:32:38 +0200

    fix: organize all files

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -22,6 +22,7 @@
       rustup
       spotify
       starship
+      stow
       zellij
     ];
 
---
+++ home/.config/fish/conf.d/nix.fish
@@ -0,0 +1,2 @@
+
+if test -e /home/yoolayn/.nix-profile/etc/profile.d/nix.fish; . /home/yoolayn/.nix-profile/etc/profile.d/nix.fish; end # added by Nix installer
