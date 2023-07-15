cb63abc72a3b44e6bd4db4517d82937340624090
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-15 17:03:25 +0200

    fix: path for coursier executables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,6 @@
 starship init fish | source
 fenv source ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+
+# >>> coursier install directory >>>
+set -gx PATH "$PATH:/home/yoolayna/.local/share/coursier/bin"
+# <<< coursier install directory <<<
