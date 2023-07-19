5bf2bfd795fa396cdc1e1da55513f3e47a4b0448
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-19 13:20:51 +0200

    fix: stow

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ cd ./home-manager/
 ./nix.sh
 cd ..
 bash home-manager/home.sh
+bash scripts/font.sh
 fish scripts/fish.sh
+bash scripts/prestow.sh
 bash scripts/stow.sh
 fish scripts/bob-nvim.sh
-bash scripts/font.sh
