a0711b329136927c717643d0f52018817e635c6c
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-22 17:42:30 +0200

    feat: lazygit alias to open dotfiles

---
+++ .config/fish/functions/dotfiles.fish
@@ -0,0 +1,4 @@
+function dotfiles --wraps='lazygit -p ~/Repositories/dotfiles' --description 'alias dotfiles lazygit -p ~/Repositories/dotfiles'
+  lazygit -p ~/Repositories/dotfiles $argv
+        
+end
