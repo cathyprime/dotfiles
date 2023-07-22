e31945763d2bca283b9b14138764a3c4d101dbeb
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-22 16:39:24 +0200

    fix: fish path

--- scripts/bob-nvim.sh
+++ scripts/bob-nvim.sh
@@ -3,7 +3,7 @@
 rustup default stable
 cargo install --git https://github.com/MordechaiHadad/bob.git
 
-fish_add_path ~/.local/share/bob/nvim-bin/
-fish_add_path ~/.cargo/bin/
+set -Ua fish_user_paths ~/.local/share/bob/nvim-bin/
+set -Ua fish_user_paths ~/.cargo/bin/
 bob install latest
 bob use latest
