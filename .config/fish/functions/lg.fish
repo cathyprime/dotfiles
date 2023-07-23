6ed1ac30d775a666fbb9281e2e7a47a1e3f42d8b
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-24 01:43:10 +0200

    fix(fish): lazygit function

--- .config/fish/functions/lg.fish
+++ .config/fish/functions/lg.fish
@@ -1,6 +1,8 @@
 function lg --wraps='lazygit -p' --description 'alias lg lazygit -p'
-    if count $argv >/dev/null
+    if count $argv >/dev/null && test -e $argv[1]
         lazygit -p $argv
+    else if count $argv >/dev/null
+        lazygit $argv
     else
         lazygit
     end
