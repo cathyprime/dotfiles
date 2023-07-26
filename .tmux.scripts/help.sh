46bcc4207ca3552a153edef1817663b23e57be1c
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-26 18:52:39 +0200

    feat(tmux): display help page

--- .tmux.scripts/help.sh
+++ .tmux.scripts/help.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 selected=$(cat ~/.tmux/cht-command | fzf)
-eval $(echo $selected --help) &
-while [:]; do sleep 1; done
+tmux neww bash -c "eval $(echo $selected --help) | bat & while true; do sleep 1; done"
