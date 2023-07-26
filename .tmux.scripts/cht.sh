96dd679d776c78129afad0b357d47257693eb21c
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-26 13:39:52 +0200

    fix(tmux): bugfixing

--- .tmux.scripts/cht.sh
+++ .tmux.scripts/cht.sh
@@ -9,7 +9,7 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.tmux/cht-languages; then
 	query=$(echo $query | tr ' ' '+')
-	tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl -s cht.sh/$selected/$query | bat & read"
+	tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
-	tmux neww bash -c "curl -s cht.sh/$selected~$query | bat"
+	tmux neww bash -c "curl -s cht.sh/$selected~$query | bat & while [ : ]; do sleep 1; done"
 fi
