7523300d8f822752801d7777a4fb9e3b05aac9d8
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-19 14:45:41 +0200

    fix(tmux): help script

--- .tmux.scripts/help.sh
+++ .tmux.scripts/help.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-selected=$(cat ~/.tmux/cht-command | fzf)
+selected=$(cat ~/.tmux/cht-command | fzf | sed "s/-/ /")
 tmux neww bash -c "eval $(echo $selected --help) | bat & while true; do sleep 1; done"
