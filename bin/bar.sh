b95b744d90420d2787b7e26f00d147fe553a779e
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-22 22:34:31 +0200

    feat(dwm-bar): disable update checker

--- bin/bar.sh
+++ bin/bar.sh
@@ -15,17 +15,17 @@ cpu() {
   printf "^c$white^ ^b$grey^ $cpu_val"
 }
 
-pkg_updates() {
-  #updates=$({ timeout 20 doas xbps-install -un 2>/dev/null || true; } | wc -l) # void
+# pkg_updates() {
+  # updates=$({ timeout 20 doas xbps-install -un 2>/dev/null || true; } | wc -l) # void
   # updates=$({ timeout 20 checkupdates 2>/dev/null || true; } | wc -l) # arch
-  updates=$({ timeout 20 aptitude search '~U' 2>/dev/null || true; } | wc -l)  # apt (ubuntu, debian etc)
+  # updates=$({ timeout 20 aptitude search '~U' 2>/dev/null || true; } | wc -l)  # apt (ubuntu, debian etc)
 
-  if [ -z "$updates" ]; then
-    printf "  ^c$green^    Fully Updated"
-  else
-    printf "  ^c$green^    $updates"" updates"
-  fi
-}
+  # if [ -z "$updates" ]; then
+  #   printf "  ^c$green^    Fully Updated"
+  # else
+  #   printf "  ^c$green^    $updates"" updates"
+  # fi
+# }
 
 battery() {
   get_capacity="$(cat /sys/class/power_supply/BAT0/capacity)"
@@ -56,7 +56,7 @@ clock() {
 
 while true; do
 
-  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] # && updates=$(pkg_updates)
   interval=$((interval + 1))
 
   sleep 1 && xsetroot -name "$updates $(battery) $(brightness) $(cpu) $(mem) $(wlan) $(clock)"
