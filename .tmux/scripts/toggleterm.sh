3759694317e03ed6b01078a323620a9ce8acbbeb
Yoolayn <jaroszynskijulian@gmail.com>
2023-09-11 00:30:31 +0200

    fix(tmux): toggleterm line calculation
    toggleterm should now correctly handle all kinds of windows
    correctly rounds number of lines

--- .tmux/scripts/toggleterm.sh
+++ .tmux/scripts/toggleterm.sh
@@ -4,16 +4,16 @@ function get_main_pane() {
     echo $(tmux list-panes | sed "s#.*%\([0-9]\+\.*\).*#\1#" | sort -n | head -n1 | sed "s#\(.*\)#%\1#")
 }
 
-size="25%"
+size=0.25
 total_lines=$(tmux display-message -p "#{window_height}")
-pane_lines=$(printf "%.0f" "$(echo "$total_lines*0.25" | bc -l)")
+pane_lines=$(echo "$total_lines * $size / 1" | bc)
 
 active_window=$(tmux list-windows | grep -E "active" | sed "s#\([0-9]\+\):.*#\1#")
 
 if tmux list-windows | grep -qs "term_${active_window}_pane"; then
     term_window=$(tmux list-windows | grep "term_${active_window}_pane" | sed "s#\(.\).*#\1#")
     source_pane=$(tmux list-panes -t "$term_window" | grep "active" | sed "s#.*\(%[0-9]\+\).*#\1#")
-    tmux join-pane -s "$source_pane" -l "$size" -t "$(get_main_pane)"
+    tmux join-pane -s "$source_pane" -l "$pane_lines" -t "$(get_main_pane)"
 else
     if [ "$(tmux list-panes | wc -l)" -gt 1 ]; then
         if tmux list-panes | grep -qsE "[0-9]{2,4}x$pane_lines"; then
@@ -22,9 +22,9 @@ else
             target_win=$(echo "$target_win 4" | tr ' ' '\n' | sort -nr | head -n 1)
             tmux break-pane -ds "$paneid" -n "term_${active_window}_pane" -t "$target_win"
         else
-            tmux split-window -l "$size" -t $(get_main_pane) -c "#{pane_current_path}"
+            tmux split-window -l "$pane_lines" -t $(get_main_pane) -c "#{pane_current_path}"
         fi
     else
-        tmux split-window -l "$size" -t $(get_main_pane) -c "#{pane_current_path}"
+        tmux split-window -l "$pane_lines" -t $(get_main_pane) -c "#{pane_current_path}"
     fi
 fi
