8328dd00409292a7d036245e8a8a4b0064f6000f
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-08 21:00:29 +0200

    fix(tmux): fix repl format

--- .tmux/scripts/repl.sh
+++ .tmux/scripts/repl.sh
@@ -15,8 +15,8 @@ else
 	repl_id=$(tmux showenv "REPL_${repl}" | sed "s/.*=//")
 fi
 
+IFS=''
 while read -r line; do
 	tmux send-keys -t "$repl_id" "$line"
+	tmux send-keys -t "$repl_id" Enter
 done
-
-tmux send-keys -t "$repl_id" Enter
