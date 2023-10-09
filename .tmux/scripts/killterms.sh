6bd2003d08b5d40bab8a7050424c3ac3a63aee8b
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-09 20:59:59 +0200

    refractor(tmux): killterms

--- .tmux/scripts/killterms.sh
+++ .tmux/scripts/killterms.sh
@@ -5,5 +5,5 @@ pane_lines=$(printf "%.0f" "$(echo "$total_lines*0.25" | bc -l)")
 
 terms=$(tmux list-windows | grep -E "term_[0-9]+_pane" | sed "s#\([0-9]\+\):.*#\1#")
 panes=$(tmux list-panes -a | grep -E "[0-9]{2,4}x$pane_lines" | sed "s#.*\(%[0-9]\+\).*#\1#")
-echo $terms | xargs -I{} tmux kill-window -t {}
-echo $panes | xargs -I{} tmux kill-pane -t {}
+echo "$terms" | xargs -I{} tmux kill-window -t {}
+echo "$panes" | xargs -I{} tmux kill-pane -t {}
