ff328e32e2b77e788a420cf0f474bf06f9057804
Yoolayn <jaroszynskijulian@gmail.com>
2023-11-02 17:35:24 +0100

    feat(tmux): change git grep to put hash into terminal

--- .tmux/scripts/git-grep.sh
+++ .tmux/scripts/git-grep.sh
@@ -2,11 +2,11 @@
 
 rm -f /tmp/git_clipboard
 mkfifo /tmp/git_clipboard
-( tmux neww -at 4 "git log --oneline --all\
+( tmux popup -E -h 50% -w 100% -y W -x W "git log --oneline --all\
     | fzf --preview 'git show --color=always {+1}'\
     | sed 's#\([0-9a-f]\{7\}\) .*#\1#'\
     | tr -d '\n' > /tmp/git_clipboard") &
 exec 3< /tmp/git_clipboard
 read -ru 3 hash
-tmux set-buffer "$hash"
+tmux send "$hash"
 rm /tmp/git_clipboard
