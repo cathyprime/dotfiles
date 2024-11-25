82c874ade03b0ae9d8366e076a89312ddf572906
cathyprime <magdajaroszynska47@gmail.com>
2024-11-25 01:02:12 +0100

    remove(zsh): nix completions

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -5,7 +5,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 mkdir completion
 pushd completion
-git clone https://github.com/nix-community/nix-zsh-completions
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 popd
 popd
